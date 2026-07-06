a644fd5add312192e1af9201e8a7b6e626aba878
Malek Miled <ge48bet@mytum.de>
2026-07-06 19:40:44 +0200

    [FIX] Use the new path to the training data

--- scripts/make_patient_splits.py
+++ scripts/make_patient_splits.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-INPUT_CSV = "data/training_data.csv"
+INPUT_CSV = "data/raw/training_data.csv"
 OUTPUT_CSV = "patient_splits.csv"
 
 TEST_SIZE = 0.15
